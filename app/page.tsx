9a546346acd13c02b29a6b96b93ecfdde8397eb3
MD. ABDUL AZIZ <md.abdulaziz6236@gmail.com>
2026-07-29 09:40:20 +0600

    feat(ui, theme): add shadcn/ui and dark mode support

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
+import { ModeToggle } from "@/components/theme/toggleButton";
 
 export default function Home() {
   return <>
-  <h1>Hello Gear Up Frontend Developer</h1>
+  <h1 className="text-2xl dark:text-5xl dark:text-green-400 text-accent-foreground">Hello Gear Up Frontend Developer</h1>
+  <ModeToggle></ModeToggle>
   </>
 }
